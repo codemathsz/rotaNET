d5c07e8a7941fc53941cc2d6bb49180a29f5db45
codemathsz <mo284891@gmail.com>
2025-10-11 14:45:21 -0300

    add GlobalAlertProvider

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import Routes from './routes';
+import { GlobalAlertProvider } from './contexts/GlobalAlertContext';
 
 export default function App() {
   return (
-    <SafeAreaView className="flex-1">
-      <Routes />
-      <StatusBar style="light" />
+    <SafeAreaView className="flex-1 bg-background-primary relative">
+      <GlobalAlertProvider>
+        <Routes />
+      </GlobalAlertProvider>
+      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
